8c04fa7116df527f7395e47bcb60dbace7a29278
David J. Felix <felix.davidj@gmail.com>
2018-05-09 20:26:50 -0400

    Complete second app

---
+++ 002/README.md
@@ -0,0 +1,15 @@
+# 001 Signup
+
+Prompt: Hint: Design a sign up page, modal, form, app screen, etc. (It's up to you!) Don't forget to share on Dribbble and/or Twitter when you're done.
+
+# Explaination
+
+I used create react app, tailwind css and some blog posts to attempt to recreate the iotv signin
+
+Thanks to 
+
+* Mark Chandler, for inspiration to start this and some good idea starting points I jacked.
+* [Hero Patterns](https://www.heropatterns.com)
+* Tailwind css
+* [Mike Francis' blog post](https://medium.com/@mikeeeeeeey/create-react-app-tailwind-css-feat-postcss-631d9e33ba8c)
+
---
+++ 002/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { CardElement, Elements, StripeProvider } from 'react-stripe-elements'
+
+import './index.css'
+
+const App = () => (
+  <StripeProvider apiKey='pk_test_YfhfHKzhltlt5vqq6shm3kZt'>
+    <Elements>
+      <div className="root001">
+        <div className='container mx-auto h-full flex justify-center items-center'>
+          <div className='sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
+            <h1 className='font-thin text-grey-darkest mb-6 text-center'>Support this Creator</h1>
+            <div className='border-blue p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg'>
+              <div className='mb-4'>
+                <label
+                  className='font-bold text-grey-darker block mb-2'
+                >Credit Card</label>
+                <CardElement
+                  className='block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow'
+                />
+              </div>
+              <button
+                className='flex bg-blue-dark hover:bg-blue text-white font-bold py-2 px-4 rounded'
+              >Pay $25</button>
+            </div>
+            <div className='text-center'>
+              <p
+                className='text-grey-dark text-sm'
+              >Secured with <a
+                className='no-underline text-blue font-bold'
+                href='https://stripe.com'
+                rel='noopener'
+                target='blank'
+              >Stripe</a>.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Elements>
+  </StripeProvider>
+)
+
+export default App
