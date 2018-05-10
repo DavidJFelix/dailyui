265bb432dece1f4c092d938fbb419bba561613f7
David J. Felix <felix.davidj@gmail.com>
2018-05-09 20:27:46 -0400

    Fix some minor issues with 001

--- 001/src/App.js
+++ 001/src/App.js
@@ -4,8 +4,8 @@ import './index.css'
 const App = () => (
   <div className="root001">
     <div className='container mx-auto h-full flex justify-center items-center'>
-      <div className='w-1/3'>
-        <h1 className='font-thin mb-6 text-center'>Create your iotv Account</h1>
+      <div className='sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
+        <h1 className='font-thin text-grey-darkest mb-6 text-center'>Create your iotv Account</h1>
         <div className='border-blue p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg'>
           <div className='mb-4'>
             <label
@@ -41,7 +41,7 @@ const App = () => (
             />
           </div>
           <button
-            className='flex bg-blue-dark hover:bg-teal text-white font-bold py-2 px-4 rounded'
+            className='flex bg-blue-dark hover:bg-blue text-white font-bold py-2 px-4 rounded'
           >Register</button>
         </div>
         <div className='text-center'>
